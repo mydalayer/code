286ce1f6a4f7250e06b323a6bea07f3f02144fee
doniefrey <mydalayer@gmail.com>
2015-03-01 10:22:40 +0800

    分页查询结果封装

---
+++ src/main/java/org/mydalayer/pagination/PaginationResult.java
@@ -0,0 +1,31 @@
+package org.mydalayer.pagination;
+
+import java.util.List;
+
+/** 
+ * 封装分页查询结果
+ * 
+ * @author mydalayer#gmail.com
+ * @version 1.0.0
+ */
+public class PaginationResult<T> {
+
+    private final List<T> result;
+
+    private final int count;
+    
+    public PaginationResult(List<T> result, int count) {
+        super();
+        this.result = result;
+        this.count = count;
+    }
+
+    public List<T> getResult() {
+        return result;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+}
